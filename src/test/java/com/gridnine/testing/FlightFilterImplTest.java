b009046d9ad6c7f8a64a63d73e3a9bbd69b8b8ef
KoryakinGG <koryaking@mail.ru>
2021-05-01 07:46:23 +0300

    fixed the names in the tests

--- src/test/java/com/gridnine/testing/FlightFilterImplTest.java
+++ src/test/java/com/gridnine/testing/FlightFilterImplTest.java
@@ -1,27 +1,32 @@
 package com.gridnine.testing;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FlightFilterImplTest {
 
-    private FlightFilter flightFilter = new FlightFilterImpl();
-    private FlightBuilder flightBuilder = new FlightBuilder();
-    private List<Flight> testFlightList = flightBuilder.createFlights();
+    FlightFilter flightFilter = new FlightFilterImpl();
+    FlightBuilder flightBuilder = new FlightBuilder();
+
 
     @Test
     void departuresUpToCurrentTime_takeFlightsArray_thenOk() {
+        List<Flight> testFlightList = flightBuilder.createFlights();
         assertEquals(1,flightFilter.departuresUpToCurrentTime(testFlightList).size());
     }
 
     @Test
     void segmentsWithArrivalDateEarlierThanDepartureDate_takeFlightsArray_thenOk() {
+        List<Flight> testFlightList = flightBuilder.createFlights();
         assertEquals(1,flightFilter.segmentsWithArrivalDateEarlierThanDepartureDate(testFlightList).size());
     }
 
     @Test
     void totalTimeSpentOnEarthExceedsTwoHours_takeFlightsArray_thenOk() {
+        List<Flight> testFlightList = flightBuilder.createFlights();
         assertEquals(2,flightFilter.totalTimeSpentOnEarthExceedsTwoHours(testFlightList).size());
     }
 }
